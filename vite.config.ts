68739248a126a33778b8442676d4bbd61b7a7ba1
Georgia Magaliou <66247466+gewmag@users.noreply.github.com>
2025-07-16 17:47:36 +0300

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-base: mode === 'production' ? '/gusto-food-scanner/' : '/',
+  //base: mode === 'production' ? '/gusto-food-scanner/' : '/',
+base: '/',
   server: {
     host: "::",
     port: 8080,
